3cb35458216a24e1a70a910e17b142604fd7b33d
Kristofer <kris@youngers.org>
2022-04-13 08:55:16 -0400

    start of working on Postgres heroku support

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 export DATABASE_URL="sqlite:///data/database.db"
 export SQLALCHEMY_DATABASE_URI="sqlite:///database.db"
-export PORT=5000
+export PORT=5005
 export SECRET_KEY="kristofer"
 honcho start
