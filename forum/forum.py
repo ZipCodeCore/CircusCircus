74980e5d5613d54c5eea2ad52f52f0e151f1c7da
Kristofer <kris@youngers.org>
2022-04-13 14:28:38 -0400

    postgressql

--- forum/forum.py
+++ forum/forum.py
@@ -14,6 +14,13 @@ from flask_login.login_manager import LoginManager
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
+# adding db url
+import os
+if os.getenv("DATABASE_URL"):
+	app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv['DATABASE_URL']
+else:
+	print("DATABASE_URL is not set, using sqlite")
+
 db = SQLAlchemy(app)
 
 #VIEWS
