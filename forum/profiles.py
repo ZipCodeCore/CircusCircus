19c4d6cdf73bfbbd945a226dc7ec142b177a5f5b
Mike <mike@ZipcodersMBP83.fios-router.home>
2023-11-25 00:07:53 -0500

    post and comment counts

--- forum/profiles.py
+++ forum/profiles.py
@@ -4,6 +4,7 @@ from flask_login import UserMixin, current_user, login_manager, login_user, logi
 from werkzeug.security import generate_password_hash, check_password_hash
 from forum.app import db, login_manager, app
 from forum.model import User, Post, Comment
+from sqlalchemy import select, func
 from forum.utl import username_taken, email_taken, valid_username
 
 """ User Profile Base View """
@@ -14,10 +15,20 @@ def user_profile(username):
     user_id = current_user.id
     posts = Post.query.filter_by(user_id=user_id).all()
     comments = Comment.query.filter_by(user_id=user_id).all()
-    return render_template('profile.html', user=user, posts=posts, comments=comments, myFunction=get_post_title)
+    return render_template('profile.html', user=user, posts=posts, comments=comments, titleFunc=get_post_title, countFunc=get_count)
 
 """ Function to get post title for a comment, used in comment title in profile.html """
 def get_post_title(post_id):
     comment_post = db.session.query(Post).filter(Post.id == post_id).first()
     comment_post_title = comment_post.title
     return comment_post_title
+
+""" Funciton to get the count of a type of forum message """
+def get_count(count_type):
+    if count_type == "post":
+        count = Post.query.filter_by(user_id=current_user.id).count()
+    elif count_type == "comment":
+        count = Comment.query.filter_by(user_id=current_user.id).count()
+    else:
+        count = 0
+    return count
