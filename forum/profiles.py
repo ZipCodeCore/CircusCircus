78241e0de936a3cf41f3e83e80f9842a1be17ba0
Mike <mike@ZipcodersMBP83.fios-router.home>
2023-11-24 22:50:52 -0500

    profile page wrapping user settings

--- forum/profiles.py
+++ forum/profiles.py
@@ -6,6 +6,7 @@ from forum.app import db, login_manager, app
 from forum.model import User, Post, Comment
 from forum.utl import username_taken, email_taken, valid_username
 
+""" User Profile Base View """
 @login_required
 @app.route('/user/<username>')
 def user_profile(username):
@@ -13,6 +14,10 @@ def user_profile(username):
     user_id = current_user.id
     posts = Post.query.filter_by(user_id=user_id).all()
     comments = Comment.query.filter_by(user_id=user_id).all()
-    return render_template('profile.html', user=user, posts=posts, comments=comments)
+    return render_template('profile.html', user=user, posts=posts, comments=comments, myFunction=get_post_title)
 
-
+""" Function to get post title for a comment, used in comment title in profile.html """
+def get_post_title(post_id):
+    comment_post = db.session.query(Post).filter(Post.id == post_id).first()
+    comment_post_title = comment_post.title
+    return comment_post_title
